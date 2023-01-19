9035d25b06d06c42bdbbbe62005987e85528fcb6
Jyoti <jyotikiran12@gmail.com>
2023-01-19 20:32:48 +0530

    Added configuration class

--- src/main/java/com/testvagrant/config/Configuration.java
+++ src/main/java/com/testvagrant/config/Configuration.java
@@ -1,4 +1,27 @@
 package com.testvagrant.config;
 
-public class Configuration {
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public final class  Configuration {
+
+    private static Configuration INSTANCE;
+    private final Properties properties = new Properties();
+    private Configuration() throws IOException {
+        FileInputStream fileInputStream=new FileInputStream("src/test/resources/application.properties");
+        properties.load(fileInputStream);
+    }
+
+    public static Configuration getInstance() throws IOException {
+        if(INSTANCE == null)
+            INSTANCE = new Configuration();
+        return INSTANCE;
+    }
+
+    public String getProperty(String key) {
+        return INSTANCE.properties.getProperty(key);
+    }
+
+
 }
